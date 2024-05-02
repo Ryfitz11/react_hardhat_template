7f8fe9957ec0487f5024098cc86a2b6d6e7c1df7
Ryan Fitzgerald <ryfitz@ymail.com>
2024-05-02 16:35:57 -0600

    List Proposals

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { ethers } from 'ethers'
 
 // Components
 import Navigation from './Navigation';
+import Proposals from './Proposals'
 import Loading from './Loading';
 
 // ABIs: Import your contract ABIs here
@@ -13,15 +14,21 @@ import DAO_ABI from '../abis/DAO.json'
 import config from '../config.json';
 
 function App() {
-  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
   const [dao, setDao] = useState(null)
   const [treasuryBalance, setTreasuryBalance] = useState(0)
+  
+  const [account, setAccount] = useState(null)
+
+  const [proposals, setProposals] = useState(null)
+  const [quorum, setQuorum] = useState(null)
 
   const [isLoading, setIsLoading] = useState(true)
 
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
 
     //Initiate contracts
     const dao = new ethers.Contract(config[31337].dao.address, DAO_ABI, provider)
@@ -35,6 +42,18 @@ function App() {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
+
+    const count = await dao.proposalCount()
+    const items = []
+
+    for (var i = 0; i < count; i++) {
+      const proposal = await dao.proposals(i + 1)
+      items.push(proposal)
+    }
+
+    setProposals(items)
+
+    setQuorum(await dao.quorum())
 
     setIsLoading(false)
   }
@@ -62,7 +81,12 @@ function App() {
 
         <hr/>
 
-
+        <Proposals 
+          provider={provider} 
+          dao={dao} 
+          proposals={proposals} 
+          quorum={quorum} 
+          setIsLoading={setIsLoading}/>
         </>
       )}
     </Container>
